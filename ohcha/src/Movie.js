ef5ee4f6327f90d681bd9fb5838c932c2911b59e
hyesungoh <haesungoh414@gmail.com>
2020-11-13 00:30:07 +0900

    MovieWeb 1.0 ~ 2.0

---
+++ ohcha/src/Movie.js
@@ -0,0 +1,11 @@
+
+function Movie() {
+    return (
+        <div class="something">
+            <h3> HELLO IM MOVIE </h3>
+            <p style={{color: 'red'}}> is it work? </p>
+        </div>
+    )
+}
+
+export default Movie;
